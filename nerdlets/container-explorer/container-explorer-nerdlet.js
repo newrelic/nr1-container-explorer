6e2971974d893e9291fe01f5ab30cf71718cf44a
DevFreddy <michael.frederickjr@gmail.com>
2019-09-14 12:51:55 -0400

    fix(NR1 SDK v2.3? upcoming breaking changes) - Wrapping launchers in component to preserve current launcherUrlState and nerdletUrlState functionality

---
+++ nerdlets/container-explorer/container-explorer-nerdlet.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Spinner } from 'nr1'
+
+import quote from '../../lib/quote'
+import nrdbQuery from '../../lib/nrdb-query'
+import findRelatedAccountsWith from '../../lib/find-related-account-with'
+
+import ContainerExplorer from './container-explorer'
+import Header from './header'
+
+export default class ContainerExplorerNerdlet extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.addFilter = this.addFilter.bind(this)
+    this.removeFilter = this.removeFilter.bind(this)
+    this.setAccount = this.setAccount.bind(this)
+
+    this.state = {
+      filters: []
+    }    
+  }
+
+  async componentDidMount() {
+    const find = {eventType: "ProcessSample", where: "containerId IS NOT NULL", 
+        timeWindow: "SINCE 1 minute ago"}
+    const accounts = await findRelatedAccountsWith(find)
+    await this.setState({accounts, account: accounts[0]})
+
+    if(accounts.length > 0) {
+      this.countProcesses()      
+    }
+  }
+
+  async addFilter(name, value) {
+    const {filters} = this.state
+    filters.push({name, value})
+    await this.setFilters(filters)
+  }
+
+  async removeFilter(name, value) {
+    let {filters} = this.state
+    
+    filters = filters.filter(f => !(f.name == name && f.value == value))
+    this.setFilters(filters)
+  }
+
+  async setFilters(filters) {
+    let where = null
+    if(filters != null && filters.length > 0) {
+      where = filters.map(({name, value}) => `${quote(name)} = '${value}'`).join(" AND ")
+    }
+
+    await this.setState({filters, where})
+    this.countProcesses()
+  }
+
+  async setAccount(account) {    
+    await this.setState({account, filters: [], where: null, counts: null})
+    this.countProcesses()
+  }
+
+  async countProcesses() {
+    this.setState({counts: null})
+
+    const timeWindow = "SINCE 30 seconds ago"
+    const {account, where} = this.state
+
+    if(!account) return
+
+    const whereClause = where ? `WHERE ${where}` : ""
+    const select = `uniqueCount(entityAndPid) as processes, uniqueCount(entityGuid) as hosts, uniqueCount(containerId) AS containers`
+    const nrql = `SELECT ${select} FROM ProcessSample ${whereClause} ${timeWindow}`
+    const counts = (await nrdbQuery(account.id, nrql))[0]
+    this.setState({counts})
+  }
+
+  render() {
+    const {account, counts, accounts} = this.state
+    if(!account) return <Spinner/>
+    if(accounts.length == 0) return <div>
+      <h1>No Data</h1>
+      <p>
+        Could not find any infrastructure data with container instrumentation. 
+        Install New Relic Infrastructure today!
+      </p>
+    </div>
+
+    return <div style={{height: "100%"}}>
+      <Header {...this.state} setAccount={this.setAccount} 
+          showFacetPicker={this.showFacetPicker} removeFilter={this.removeFilter}/>
+      {counts && <ContainerExplorer {...this.state} addFilter={this.addFilter}/>}
+    </div>
+  }
+}
---
+++ nerdlets/service-containers/service-containers.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { EntityByGuidQuery, Grid, GridItem, Spinner, EntityStorageQuery, EntityStorageMutation } from 'nr1'
+
+import nrdbQuery from '../../lib/nrdb-query'
+import timePickerNrql from '../../lib/time-picker-nrql'
+import findRelatedAccountsWith from '../../lib/find-related-account-with'
+import accountsWithData from '../../lib/accounts-with-data'
+
+import ContainerPanel from '../shared/container-panel'
+import ContainerHeatMap from './heat-maps'
+import NoInfrastructureData from './no-infra-data'
+
+
+export default class ServiceContainers extends React.Component {
+  static propTypes = {
+    nerdletUrlState: PropTypes.object,
+    launcherUrlState: PropTypes.object,
+    width: PropTypes.number,
+    height: PropTypes.number,
+  };
+
+  constructor(props) {
+    super(props)
+
+    this._selectContainer = this._selectContainer.bind(this)
+    this.state = {}
+  }
+
+  componentDidMount() {
+    this.findInfraAcount()
+  }
+  
+  componentDidUpdate({nerdletUrlState }) {
+    if(nerdletUrlState.entityGuid != this.props.nerdletUrlState.entityGuid) {
+      this.findInfraAcount()
+    }
+  }
+
+  _selectContainer(containerId) {
+    this.setState({ containerId })
+  }
+
+  async findInfraAcount() {
+    const { entityGuid } = this.props.nerdletUrlState || {}
+    const timeRange = timePickerNrql(this.props)
+
+    let result = await EntityByGuidQuery.query({ entityGuid })
+    const entity = result.data.entities[0]
+    const nrql = `SELECT uniques(containerId) FROM Transaction 
+        WHERE entityGuid = '${entityGuid}' ${timeRange}`
+
+    // get the container id's that this app runs in
+    result = await nrdbQuery(entity.accountId, nrql)
+    const containerIds = result.map(r => r.member)
+    this.setState({ containerIds })
+
+    // look up the infrastucture account(s) that are associated with this entity.
+    // cache for performance.
+    const storageQuery = { collection: "GLOBAL", entityGuid, documentId: "infraAccounts" }
+    const storageResult = await EntityStorageQuery.query(storageQuery)
+
+    console.log(storageResult)
+    let infraAccounts = storageResult.data
+
+    // find the account(s) that are monitoring these containers. Hopefully there's exactly
+    // one, but not, take the account with the most matches.
+    if (!infraAccounts && containerIds && containerIds.length > 0) {
+      const where = `containerId IN (${containerIds.map(cid => `'${cid}'`).join(',')})`
+      find = { eventType: 'ProcessSample', where }
+
+      infraAccounts = await findRelatedAccountsWith(find)
+
+      // cache in entity storage
+      storageQuery.document = infraAccounts
+      storageQuery.actionType = EntityStorageMutation.ACTION_TYPE.WRITE_DOCUMENT
+
+      await EntityStorageMutation.mutate(storageQuery)
+    }
+
+    if (infraAccounts.length == 0) {
+      const searchedAccounts = await accountsWithData("ProcessSample")
+      this.setState({ accountDataNotFound: true, searchedAccounts, entity })      
+    }
+    else {
+      // use the infra account with the most hits as the primary for this entity, and then
+      // store the others otherInfraAccounts so we can show an info box.
+      this.setState({ infraAccount: infraAccounts[0], allInfraAccounts: infraAccounts , containerIds, entity, timeRange})
+    }
+  }
+
+  render() {
+    // workaround for bug
+    if (this.props.timeRange) return <div />
+
+    const { infraAccount, containerId, entity, timeRange, accountDataNotFound, searchedAccounts } = this.state
+
+    if (accountDataNotFound) {
+      return <NoInfrastructureData accounts={searchedAccounts} entity={entity} />
+    }
+
+    if (!entity || !infraAccount) return <Spinner fillContent style={{ width: "100%", height: "100%" }} />
+
+    return <div id="root">
+      <Grid style={{ height: "100%" }}>
+        <GridItem columnSpan={7}>
+          {/* <ContainerTable {...this.state} selectContainer={this._selectContainer} timeRange={timeRange}/> */}
+          <ContainerHeatMap {...this.state} selectContainer={infraAccount && this._selectContainer} />
+        </GridItem>
+        <GridItem className="content" columnSpan={5}>
+          {!infraAccount && <div/>} 
+          {infraAccount && containerId && <ContainerPanel
+            account={infraAccount}
+            containerId={containerId}
+            onClose={() => this.setState({ containerId: null })}
+            timeRange={timeRange} />}
+        </GridItem>
+      </Grid>
+    </div>
+  }
+}
