a9f89410418ee3014557b39d5f733d5fbd4ef133
Daniel Golden <dgolden@newrelic.com>
2019-10-28 07:18:49 -0400

    fix: reintroduce omitted sidebar functionality
    Before PR #19, when the details sidebar was active
    users had two buttons to either open the active
    container in a stacked page or directly in the entity
    explorer. That functionality was removed in #19, and
    It is reintroduced in this commit.

--- nerdlets/shared/linked-entity.js
+++ nerdlets/shared/linked-entity.js
@@ -1,5 +1,26 @@
 import { Button, navigation } from 'nr1'
 
-export default function LinkedEntity({ title, entity, name, icon }) {
-  return <a className="entity-explorer-link" href="#" onClick={() => navigation.openStackedEntity(entity.guid)}>View in Entity Explorer</a>
+export default function LinkedEntity({ entity, icon }) {
+  return (
+    <div className="section">
+      <Button
+        sizeType={Button.SIZE_TYPE.SMALL}
+        type={Button.TYPE.PLAIN}
+        iconType={icon}
+        onClick={() => navigation.openStackedEntity(entity.guid)}
+        className="entity-explorer-modal-link"
+      >
+        View in Modal
+      </Button>
+      <Button
+        sizeType={Button.SIZE_TYPE.SMALL}
+        type={Button.TYPE.PLAIN}
+        iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__DRAG}
+        onClick={() => navigation.openEntity(entity.guid)}
+        className="entity-explorer-ee-link"
+      >
+        View in Entity Explorer
+      </Button>
+    </div>
+  )
 }
