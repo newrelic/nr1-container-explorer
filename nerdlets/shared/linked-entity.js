a8150aae4a02cad033036a68b093b06e0eba3e7f
DevFreddy <michael.frederickjr@gmail.com>
2020-01-13 14:08:06 -0500

    fix: update links for container related apps

--- nerdlets/shared/linked-entity.js
+++ nerdlets/shared/linked-entity.js
@@ -1,24 +1,27 @@
-import { Button, navigation } from 'nr1'
+import { Button, navigation, Tooltip } from 'nr1'
 
-export default function LinkedEntity({ entity, icon }) {
+export default function LinkedEntity({ entity, name, title, icon }) {
   return (
     <div className="section">
-      <Button
-        sizeType={Button.SIZE_TYPE.SMALL}
-        type={Button.TYPE.PLAIN}
-        onClick={() => navigation.openStackedEntity(entity.guid)}
-        className="entity-explorer-modal-link"
-      >
-        View in Modal
-      </Button>
-      <Button
-        sizeType={Button.SIZE_TYPE.SMALL}
-        type={Button.TYPE.PLAIN}
-        onClick={() => navigation.openEntity(entity.guid)}
-        className="entity-explorer-ee-link"
-      >
-        View in Entity Explorer
-      </Button>
+      <Tooltip text={'View in Modal'}>
+        <Button
+          sizeType={Button.SIZE_TYPE.SMALL}
+          iconType={Button.ICON_TYPE.HARDWARE_AND_SOFTWARE__SOFTWARE__SERVICE}
+          onClick={() => navigation.openStackedEntity(entity.guid)}
+          className="entity-explorer-modal-link"
+        >
+          {name}
+        </Button>
+      </Tooltip>
+      <Tooltip text={'View in Entity Explorer'}>
+        <Button
+          sizeType={Button.SIZE_TYPE.SMALL}
+          iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__DRAG}
+          onClick={() => navigation.openEntity(entity.guid)}
+          className="entity-explorer-ee-link"
+        >
+        </Button>
+      </Tooltip>
     </div>
   )
 }
